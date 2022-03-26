ee9cbb511c1cbdb2ef26a7735d578ba970460e82
sandesh sharma <sharmamasandesh@gmail.com>
2022-03-26 08:45:15 +0545

    latestChange

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-//for slider
-var width =
-  window.innerWidth ||
-  document.documentElement.clientWidth ||
-  document.body.clientWidth;
+// //for slider
+// var width =
+//   window.innerWidth ||
+//   document.documentElement.clientWidth ||
+//   document.body.clientWidth;
 
-const sliderBtnRight = document.querySelector(".ourteamright");
-const sliderBtn = document.querySelector(".ourteamleft");
-const imageBox = document.querySelectorAll(".main-ourteam__imageBox");
-const imageBoxLength =
-  document.querySelectorAll(".main-ourteam__imageBox").length - 4;
-let R = 0;
+// const sliderBtnRight = document.querySelector(".ourteamright");
+// const sliderBtn = document.querySelector(".ourteamleft");
+// const imageBox = document.querySelectorAll(".main-ourteam__imageBox");
+// const imageBoxLength =
+//   document.querySelectorAll(".main-ourteam__imageBox").length - 4;
+// let R = 0;
 
-sliderBtnRight.addEventListener("click", (event) => {
-  console.log(R);
-  if (
-    +imageBox[0].style.left.slice(0, imageBox[0].style.left.length - 2) <
-    -(imageBoxLength * 400)
-  ) {
-    return;
-  }
+// sliderBtnRight.addEventListener("click", (event) => {
+//   console.log(R);
+//   if (
+//     +imageBox[0].style.left.slice(0, imageBox[0].style.left.length - 2) <
+//     -(imageBoxLength * 400)
+//   ) {
+//     return;
+//   }
 
-  R++;
+//   R++;
 
-  for (let item of imageBox) {
-    if (R <= imageBoxLength) {
-      let leftNum = +item.style.left.slice(0, item.style.left.length - 2);
+//   for (let item of imageBox) {
+//     if (R <= imageBoxLength) {
+//       let leftNum = +item.style.left.slice(0, item.style.left.length - 2);
 
-      leftNum -= width;
+//       leftNum -= width;
 
-      item.style.left = `${leftNum}px`;
-    }
-  }
-});
+//       item.style.left = `${leftNum}px`;
+//     }
+//   }
+// });
 
-sliderBtn.addEventListener("click", (event) => {
-  console.log(R);
-  console.log(
-    +imageBox[0].style.left.slice(0, imageBox[0].style.left.length - 2)
-  );
-  if (+imageBox[0].style.left.slice(0, imageBox[0].style.left.length - 2) > 0) {
-    return;
-  }
-  if (R === 0) {
-    return;
-  }
-  R--;
+// sliderBtn.addEventListener("click", (event) => {
+//   console.log(R);
+//   console.log(
+//     +imageBox[0].style.left.slice(0, imageBox[0].style.left.length - 2)
+//   );
+//   if (+imageBox[0].style.left.slice(0, imageBox[0].style.left.length - 2) > 0) {
+//     return;
+//   }
+//   if (R === 0) {
+//     return;
+//   }
+//   R--;
 
-  for (let item of imageBox) {
-    if (R >= 0) {
-      let leftNum = +item.style.left.slice(0, item.style.left.length - 2);
-      console.log(leftNum);
-      leftNum += width;
-      console.log(leftNum);
-      item.style.left = `${leftNum}px`;
-      console.log(item.style.left);
-    }
-  }
-});
+//   for (let item of imageBox) {
+//     if (R >= 0) {
+//       let leftNum = +item.style.left.slice(0, item.style.left.length - 2);
+//       console.log(leftNum);
+//       leftNum += width;
+//       console.log(leftNum);
+//       item.style.left = `${leftNum}px`;
+//       console.log(item.style.left);
+//     }
+//   }
+// });
